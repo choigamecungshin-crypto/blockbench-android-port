212f950448cb75bca2b7f8ab618753d9353309dd
JannisX11 <jannis4236@gmail.com>
2026-09-09 19:58:03 +0200

    Disable constant outlines on texture meshes

--- js/outliner/outliner.js
+++ js/outliner/outliner.js
@@ -340,7 +340,7 @@ export class NodePreviewController extends EventSystem {
 			if (Modes.paint && settings.outlines_in_paint_mode.value === false) {
 				mesh.outline.visible = false;
 			} else {
-				mesh.outline.visible = element.selected || settings.constant_outlines.value;
+				mesh.outline.visible = element.selected || (settings.constant_outlines.value && !(element instanceof TextureMesh));
 				if (mesh.outline.material == Canvas.outlineMaterial || mesh.outline.material == Canvas.outlineUnselectedMaterial) {
 					mesh.outline.material = element.selected ? Canvas.outlineMaterial : Canvas.outlineUnselectedMaterial;
 				}
